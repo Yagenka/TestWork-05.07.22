3da382cfe9020a2ae1c7584c001b0bdf2acbf432
Tamara <tomritol@gmail.com>
2022-07-05 08:36:45 +0300

    создание и заполение исходного массива

---
+++ Task/Program.cs
@@ -0,0 +1,25 @@
+﻿// Создание массива исходных строк
+// Основная задача
+// Печать массива
+
+Console.Write("Введите количество элементов в массиве: ");
+int len = Convert.ToInt32(Console.ReadLine());
+string[] newarray = CreateArray(len);
+FillArray(newarray);
+
+
+string[] CreateArray(int count)
+{
+    return new string[count];
+}
+
+void FillArray(string[] inputArray)
+{
+    int len = inputArray.Length;
+    Console.WriteLine($"Введите {len} элементов массива");
+    for (int i = 0; i < len; i++)
+    {
+        inputArray[i] = Console.ReadLine();
+    }
+}
+
